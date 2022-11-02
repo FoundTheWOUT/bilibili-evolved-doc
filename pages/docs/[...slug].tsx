9d18a5c28ee332a7ce30b10a1ca916a96df84b3b
FoundTheWOUT <waua@outlook.com>
2022-11-02 21:54:12 +0800

    Fix 404 flashing

--- pages/docs/[...slug].tsx
+++ pages/docs/[...slug].tsx
@@ -7,26 +7,24 @@ import { useRouterTree } from "hooks/useRouterTree";
 import Loading from "components/Loading";
 import Error from "next/error";
 
+const LoadingComp = (
+  <div className="h-[90vh] w-full flex justify-center items-center">
+    <Loading className="h-20 text-MAIN" />
+  </div>
+);
+
 const MDXPage = () => {
   const path = useMdxPath();
   const { tree: routerTree, getNode } = useRouterTree();
   const Comp = getNode(path)?.Comp;
-
-  if (!path || !Comp) return <Error statusCode={404} />;
 
   return (
     <SidebarProvider>
       <NavBar />
       <SideBar routerTree={routerTree} />
       <div className="px-6 lg:flex lg:ml-80">
-        <Suspense
-          fallback={
-            <div className="h-[90vh] w-full flex justify-center items-center">
-              <Loading className="h-20 text-MAIN" />
-            </div>
-          }
-        >
-          <Comp components={MDXComponents} />
+        <Suspense fallback={LoadingComp}>
+          {Comp ? <Comp components={MDXComponents} /> : LoadingComp}
         </Suspense>
       </div>
     </SidebarProvider>
